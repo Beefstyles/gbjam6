4a950e1235dd8ed5fa842a82da2189c3037afa67
Beefstyles <glenwillis@gmail.com>
2016-10-02 09:56:52 +0800

    Added a character controller with jumping.

--- gbjam6/Assets/Scripts/PlayerControllerScript.cs
+++ gbjam6/Assets/Scripts/PlayerControllerScript.cs
@@ -3,13 +3,48 @@ using System.Collections;
 
 public class PlayerControllerScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+
+    private float speed = 3.0F;
+    private float jumpSpeed = 15.0F;
+    Rigidbody2D rb;
+
+    private bool isGrounded;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+	void Update ()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        transform.position += move * speed * Time.deltaTime;
+        if (isGrounded)
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                Debug.Log("Fire");
+                rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Force);
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log("Entered ground");
+        if(coll.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D coll)
+    {
+        Debug.Log("Exited ground");
+        if (coll.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
 }
