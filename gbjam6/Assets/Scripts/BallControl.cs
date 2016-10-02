a583378993efe753c979ee59abb14596182fba8e
Beefstyles <glenwillis@gmail.com>
2016-10-02 17:09:04 +0800

    Added a random direction system.

--- gbjam6/Assets/Scripts/BallControl.cs
+++ gbjam6/Assets/Scripts/BallControl.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 public class BallControl : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    public Vector3 dir;
+    Rigidbody2D rb;
+
+	void Start ()
+    {
+        float xDir = Random.Range(-1F, 1F);
+        float yDir = Random.Range(-1F, 1F);
+        rb = GetComponent<Rigidbody2D>();
+        dir = new Vector3(xDir, yDir, 0F);
+        dir = dir.normalized;
+        rb.AddForce(dir * 50);
 	}
 	
 	// Update is called once per frame
