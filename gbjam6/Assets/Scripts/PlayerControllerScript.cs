5039aa06723f859939f2ed50f077a59212b44ee1
Beefstyles <glenwillis@gmail.com>
2016-10-02 15:58:31 +0800

    Adding pogo mechanic

--- gbjam6/Assets/Scripts/PlayerControllerScript.cs
+++ gbjam6/Assets/Scripts/PlayerControllerScript.cs
@@ -6,9 +6,11 @@ public class PlayerControllerScript : MonoBehaviour {
 
     private float speed = 3.0F;
     private float jumpSpeed = 15.0F;
+    private float pogoForce = 30F;
     Rigidbody2D rb;
 
     private bool isGrounded;
+    private bool pogoUsed;
 
 	void Start ()
     {
@@ -24,24 +26,32 @@ public class PlayerControllerScript : MonoBehaviour {
         {
             if (Input.GetButton("Fire1"))
             {
-                Debug.Log("Fire");
                 rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Force);
             }
+        }
+
+        if (!isGrounded && Input.GetButton("Fire2"))
+        {
+            pogoUsed = true;
         }
     }
 
     void OnCollisionEnter2D(Collision2D coll)
     {
-        Debug.Log("Entered ground");
         if(coll.gameObject.tag == "Ground")
         {
             isGrounded = true;
+        }
+
+        if (pogoUsed)
+        {
+            rb.AddForce(Vector2.up * pogoForce);
+            pogoUsed = false;
         }
     }
 
     void OnCollisionExit2D(Collision2D coll)
     {
-        Debug.Log("Exited ground");
         if (coll.gameObject.tag == "Ground")
         {
             isGrounded = false;
