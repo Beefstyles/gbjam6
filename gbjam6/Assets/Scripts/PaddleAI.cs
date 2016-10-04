488dfb0d4f5b3af568ac610031cd50df50e3a38a
Beefstyles <glenwillis@gmail.com>
2016-10-04 21:28:03 +0800

    AI works approximately.

--- gbjam6/Assets/Scripts/PaddleAI.cs
+++ gbjam6/Assets/Scripts/PaddleAI.cs
@@ -4,17 +4,22 @@ using System.Collections;
 public class PaddleAI : MonoBehaviour {
 
     public Transform Ball;
-    private float speed = 5;
+    private float speed = 0.1F;
+    private float initialX, initialY, initialZ;
+ 
     
     
 	// Use this for initialization
 	void Start () {
+        initialX = transform.position.x;
+        initialY = transform.position.y;
+        initialZ = transform.position.z;
         Ball = GameObject.FindGameObjectWithTag("Ball").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, Ball.position, step);
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(Ball.position.x, initialY, initialZ), step);
 	}
 }
