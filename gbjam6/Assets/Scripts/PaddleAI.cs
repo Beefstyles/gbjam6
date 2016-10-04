32e84e6154b599cf76b806f0868f874bb57adaea
Beefstyles <glenwillis@gmail.com>
2016-10-04 21:23:58 +0800

    Added some basic paddle AI.

---
+++ gbjam6/Assets/Scripts/PaddleAI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleAI : MonoBehaviour {
+
+    public Transform Ball;
+    private float speed = 5;
+    
+    
+	// Use this for initialization
+	void Start () {
+        Ball = GameObject.FindGameObjectWithTag("Ball").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, Ball.position, step);
+	}
+}
