8fc237f99d50c6081dc7776f65ab5c78b0b64f18
Beefstyles <glenwillis@gmail.com>
2016-10-09 16:35:34 +0800

    Added button handler script

---
+++ gbjam6/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour {
+
+    void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
--- gbjam6/Assets/Scripts/CarryOverInfo.cs
+++ gbjam6/Assets/Scripts/CarryOverInfo.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class CarryOverInfo : MonoBehaviour {
 
+    static int HighScore;
+
 	// Use this for initialization
 	void Start () {
-	
+        DontDestroyOnLoad(this);
 	}
 	
 	// Update is called once per frame
